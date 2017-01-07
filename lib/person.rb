6b8fc5d5e33870200ad980314192a480039e657d
Patrick Logan <jpatricklogan@gmail.com>
2017-01-07 03:47:19 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def talk
+    puts "Hello World"
+  end
+  def walk
+    puts "The Person is walking"
+  end
+  def talk
+    puts "Hello World!"
+  end
+end
+
+adele_goldberg = Person.new
+alan_kay=Person.new
+
+adele_goldberg.talk
