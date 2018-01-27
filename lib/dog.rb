c7e9fa28fca601f8dc886bb20be6f0cd48636361
louis baptiste <louisvis@ymail.com>
2018-01-27 00:58:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+## I remember when learning the concept of structs in CS50, I always wonder if
+## you could add funtions within structs ... well, now I know
+
+
+class Dog
+
+    def bark
+      puts "Woof!"
+    end
+    def sit
+      puts "The Dog is sitting"
+    end
+
+end
